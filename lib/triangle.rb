791cf32baa6b56d6fd8b28249d1db677c354eb0e
Mike Gaudreau <gaudreaum01@gmail.com>
2019-09-21 15:31:06 -0400

    Lab: Passing 10 or 15 tests

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,61 @@
+require 'pry-moves'
+
 class Triangle
-  # write code here
+
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+
+    if self.equilateral?
+      :equilateral
+    elsif self.isosceles?
+      :isosceles
+    elsif self.scalene?
+      :scalene
+    else
+      puts "Triangle Error"
+    end
+
+
+  end
+
+  def equilateral?
+    if @side_1 == @side_2 && @side_2 == @side_3
+      true
+    else
+      false
+    end
+  end
+
+  def isosceles?
+    if @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+      true
+    else
+      false
+    end
+  end
+
+  def scalene?
+    if @side_1 != @side_2 && @side_2 != @side_3
+      true
+    else
+      false
+    end
+  end
+
+
+  class TriangleError < StandardError
+
+    def message
+      "** Triangle Error: Custom error thrown! **"
+    end
+
+  end
+
 end
