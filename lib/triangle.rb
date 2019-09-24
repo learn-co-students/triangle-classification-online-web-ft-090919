ce8a9787e3db5ba9ed62f47957b527d7cc0f2673
Kenneth Spatta <kenyounot@gmail.com>
2019-09-23 20:47:43 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_accessor :first, :second, :third
+
+  def initialize(first, second, third)
+    @first = first
+    @second = second
+    @third = third
+  end
+
+  def kind
+    sides = [self.first, self.second, self.third].uniq
+    if (self.first > 0 && self.second > 0 && self.third > 0) && (self.first + self.second > self.third && self.second + self.third > self.first && self.first + self.third > self.second)
+      type = case sides.length
+              when 1 then :equilateral 
+              when 2 then :isosceles 
+              when 3 then :scalene
+            end
+    else
+      raise TriangleError
+    end
+
+  end
+
+  class TriangleError < StandardError
+  end  
+
 end
