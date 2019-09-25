d2312048eb8b2a573d4c9876e8e21f68d9d37d8e
Doug Kintop <dkintop@hotmail.com>
2019-09-25 16:46:34 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
 class Triangle
-  # write code here
-end
+  class TriangleError < StandardError
+    "defined shape is not a triangle"
+  end
+  
+  
+  attr_writer :side_1, :side_2, :side_3
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end 
+  
+  def triangle?
+     if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
+      return false
+    elsif @side_1+@side_2 <= @side_3 || @side_1+@side_3 <= @side_2 || @side_2+@side_3 <= @side_1
+      return false 
+    else 
+     return true
+    end
+  end 
+  
+  
+  def kind 
+    if self.triangle? == false
+      raise TriangleError
+    else
+      if @side_1 == @side_2 && @side_2 == @side_3
+        :equilateral
+      elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1
+        :isosceles
+      elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_3 != @side_1
+        :scalene
+      end
+    end 
+  end 
+end  
+  
+  
+  
+  
+
