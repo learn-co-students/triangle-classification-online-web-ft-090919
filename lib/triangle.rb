3cdc0e879691887e1fc5a6385065a7088ab9ea95
Matt Plichta <bigcatplichta@gmail.com>
2019-09-27 05:50:21 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,27 +8,35 @@ class Triangle
   end 
   
   def kind 
-    case 
-    when @side1 == @side2 && @side2 == @side3
-      :equilateral
-    when @side1 == @side2 && @side1 != @side3
-      :isosceles
-    when @side1 == @side3 && @side1 != @side2 
-      :isosceles
-    when @side2 == @side3 && @side1 != @side3
-      :isosceles
-    when @side1 !=@side2 && @side2 != @side3
-      :scalene 
-    when @side1 <= 0 || @side2 <= 0 || @side3 <= 0
-      raise TriangleError 
-    when @side1 > @side2 + @side3 || @side2 > @side1 = @side3
-      raise TriangleError
+ 
+    raise TriangleError if illegal?
+    return :equilateral if equilateral?
+    return :isosceles if isosceles?
+    return :scalene if scalene?
+    
+  end 
+  
+  def equilateral?
+   @side1 == @side2 && @side2 == @side3
+  end 
+  
+  def isosceles?
+    (@side1 == @side2 && @side1 != @side3) || (@side1 == @side3 && @side1 != @side2) || (@side2 == @side3 && @side1 != @side3)
+  end 
+  
+  def scalene?
+    @side1 != @side2 && @side2 != @side3
+  end 
+  
+  def illegal?
+    if @side1 == 0 || @side2 == 0 || @side3 == 0
+      true 
+    elsif @side1 >= (@side2 + @side3) || @side2 >= (@side1 + @side3) || @side3 >= (@side1 + @side2)
+      true 
     end 
   end 
   
   class TriangleError < StandardError
-    def message
-      puts "That's illegal"
-    end 
-  end 
+  end
+  
 end
