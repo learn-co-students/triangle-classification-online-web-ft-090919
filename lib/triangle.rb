351d9bcd9b371795e56deb26f85c6503577f438d
kerikh <kerid18@att.net>
2019-09-28 14:10:03 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(s1, s2, s3)
+    @triangle_sides = [s1,s2,s3]
+
+    if @triangle_sides.any? {|x| x <= 0 } 
+        raise TriangleError
+    end 
+
+    sides = @triangle_sides.sort 
+    if sides[0]+sides[1] <= sides[2]
+        raise TriangleError
+    end 
+
+  end  
+
+  def kind
+    if @triangle_sides.uniq.count == 1
+      :equilateral
+    elsif @triangle_sides.uniq.count == 3
+      :scalene
+    elsif @triangle_sides.uniq.count == 2
+      :isosceles
+    end
+  end 
+  class TriangleError < StandardError
+    def message 
+      "This is not a valid Triangle."
+    end
+  end 
 end
