014a8f83dddc62cafaa4258e84c4a8707b9f536a
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-28 09:49:45 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
-class Triangle
-  # write code here
+lass Triangle
+  attr_accessor :s1, :s2, :s3
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+  
+  def kind 
+    if s1 = s2 = s3 then
+      return :equilateral
+    end
+    if s1 != s2 and
+       s2 != s3 and 
+       s1 != s3 then
+      return :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+  
+  end
 end
