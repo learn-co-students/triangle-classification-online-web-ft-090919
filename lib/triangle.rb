ac1da78ec816496eb733e92f0a3fe89ff2f499df
Shaqqour M <mshaqqourm@gmail.com>
2019-09-29 01:59:18 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+    attr_accessor :first, :second, :third
+
+    def initialize(first, second, third)
+        @first = first
+        @second = second
+        @third = third
+    end
+
+    def kind
+        sides = [@first, @second, @third]
+        copy = sides.dup
+        if (@first + @second <= @third) || (@first + @third <= @second) || (@second + @third <= @first) || sides.any? { |side| side <= 0 }
+            raise TriangleError
+        else
+            if sides.uniq.length == 1
+                return :equilateral
+            end
+            if sides.uniq.length == 2
+                return :isosceles
+            end
+            if sides.uniq.length == 3
+                return :scalene
+            end
+        end
+    end
+
+    class TriangleError < StandardError
+        def message
+            "Invalid Triangle"
+        end
+    end
+
 end
